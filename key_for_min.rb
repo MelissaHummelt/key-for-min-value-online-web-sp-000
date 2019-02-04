ce60ddea17cadb3dd31a36551d6b9bd1bacdabc3
Melissa Hummelt <mkhummelt@gmail.com>
2019-02-04 20:35:46 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   lowest_value = nil
   name_hash.each do |key, value|
     if lowest_value == nil || value < lowest_value
-      key = lowest_key
-      value = lowest_value
+      lowest_key = key
+      owest_value = value
     end
   return lowest_key
 end
